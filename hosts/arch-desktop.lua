168bda2ab2b7fed22811ba6c5bb1948d44aad1fd
soundfox99 <66957702+soundfox99@users.noreply.github.com>
2026-08-23 13:26:53 -0400

    auto: dcli sync snapshot — 2 files changed, 2 insertions(+), 1 deletion(-)

--- hosts/arch-desktop.lua
+++ hosts/arch-desktop.lua
@@ -15,6 +15,7 @@ return {
         -- Theming, fonts, notifications
         "fonts/module",
         "theming/module",
+        "theming/wallpapers",
         "notifications/module",
 
         -- CLI + shell
